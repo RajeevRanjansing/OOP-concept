a35c74c3c73d374dca175c5586f05e601e0ad008
Rajeev Ranjan <159820641+RajeevRanjansing@users.noreply.github.com>
2024-03-26 22:52:53 +0530

    Add files via upload

---
+++ xyz.java
@@ -0,0 +1,187 @@
+// class A{
+//     int a=50;
+// }
+// class B extends A{
+//     int a=100;
+//     void show(){
+//         System.out.println(a);
+//          System.out.println(super.a);
+//     }
+// }
+// class ABC
+// {
+//     public static void main(String args[]){
+//         B b1=new B();
+//         b1.show();
+//     }
+// }
+
+// class A{
+//     void show(){
+//         System.out.print("Hello Developer");
+//     }
+// }
+// class B extends A{
+//     void show(){
+//         super.show();
+//         System.out.print("Hello Rajeev");
+//     }
+// }
+// class ABC{
+//     public static void main(String args[]){
+//         B b1=new B();
+//         b1.show();
+//     }
+// }
+
+
+
+// class A{
+//     void show(){
+//         System.out.print("hello");
+//     }
+// }
+// class B extends A{
+  
+//     void show(){
+//         super.show();
+//         System.out.println("Rajeev");
+        
+//     }
+// }
+// class ABC{
+//     public static void main(String args[]){
+//         B b1=new B();
+//         b1.show();
+//     }
+// }
+
+
+class x{
+    void show(){
+    System.out.print("CSE");
+}
+}
+
+class y extends x{
+    void show(){
+        super.show();
+    System.out.print("mech");
+}
+}
+
+class ABC{
+    public static void main(String args[]){
+        y y1=new y();
+        y1.show();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+//Definition of super keyword is 
+//super keyword is used to refer immediate super class instance variable.
+//super is used to immediate super class constructor.
+//super is used to immediate super class method.
+
+//1st program
+
+class A{
+    int a=50;
+}
+class B extends A{
+    int a=100;
+    void show(){
+        System.out.println(a);
+         System.out.println(super.a);
+    }
+}
+class xyz
+{
+    public static void main(String args[]){
+        B b1=new B();
+        b1.show();
+    }
+}
+
+//2nd program
+
+class A{
+    void show(){
+        System.out.println("Hello Developer");
+    }
+}
+class B extends A{
+    void show(){
+         System.out.print("Hello Rajeev");
+    }
+}
+class xyz{
+    public static void main(String args[]){
+        B b1=new B();
+        b1.show();                                            //output is Hello Rajeev
+    }
+}
+
+//3rd program
+
+class A{
+    void show(){
+        System.out.println("namaste");
+    }
+}
+class B extends A{
+  
+    void show(){
+        super.show();
+        System.out.println("Rajeev");
+        
+    }
+}
+class xyz{
+    public static void main(String args[]){
+        B b1=new B();
+        b1.show();                                          //output is namaste Rajeev
+    }
+}
+
+//4th program
+
+class x{
+    void show(){
+    System.out.println("CSE");
+}
+}
+
+class y extends x{
+    void show(){
+        super.show();
+    System.out.print("mech");
+}
+}
+
+class xyz{
+    public static void main(String args[]){
+        y y1=new y();
+        y1.show();                                          //output is CSE mech
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
