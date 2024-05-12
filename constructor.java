8b747378a51628344ac2640c44c67bb5f27b32bc
Rajeev Ranjan <159820641+RajeevRanjansing@users.noreply.github.com>
2024-05-12 12:43:10 +0530

    constructor.java

--- constructor.java
+++ constructor.java
@@ -1,1 +1,19 @@
+// Constructor
+//constructor is a special type of method.
+//constructor is used to initialize the object.
 
+class A{
+     int a;String name; boolean c;
+    A(){
+        a=100; name="Rajeev"; c=true;
+    }
+        void show(){
+            System.out.println(a+" "+ name +" "+c);
+        }
+}
+class B{
+    public static void main(String args[]){
+        A r=new A();
+        r.show();
+    }
+}
