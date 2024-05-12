49a04d13b19e7039106884edd4741ad4f2e005fb
Rajeev Ranjan <159820641+RajeevRanjansing@users.noreply.github.com>
2024-05-12 12:49:46 +0530

    constructor.java

--- constructor.java
+++ constructor.java
@@ -2,10 +2,32 @@
 //constructor is a special type of method.
 //constructor is used to initialize the object.
 
+
+//1st program
+
+class A{
+     int a;String name; boolean c;          //here we are not intializing the value
+        void show(){
+            System.out.println(a+" "+ name +" "+c);
+        }
+}
+class B{
+    public static void main(String args[]){
+        A r=new A();
+        r.show();
+    }                                          //constructor automatically initialize the object
+}                                              //output is 0 null false
+
+
+
+
+//2nd program
+//Default constructor
+
 class A{
      int a;String name; boolean c;
     A(){
-        a=100; name="Rajeev"; c=true;
+        a=100; name="Rajeev"; c=true;          //here we are initializing the object
     }
         void show(){
             System.out.println(a+" "+ name +" "+c);
