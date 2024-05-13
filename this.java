be8a30c233d1465da67ab4ef7af42e61fe3233d6
Rajeev Ranjan <159820641+RajeevRanjansing@users.noreply.github.com>
2024-05-14 00:47:23 +0530

    this.java

--- this.java
+++ this.java
@@ -1,1 +1,19 @@
+//this keyword is written inside the method.
+// Local variables are declared within a method or a block of code.
+//Instance variables are declared within a class but outside any method, constructor, or block.
 
+//1st program
+
+class B{
+    int a;    //instance variable
+    B(int a){    //local variable
+        this.a=a;            
+    }
+    void show(){
+        System.out.println(a);
+    }
+    public static void main(String args[]){
+        B r=new B(100);
+        r.show();
+    }
+}
