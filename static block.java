7e1453dee4ea8e1fa765942419ff21d45e8975db
Rajeev Ranjan <159820641+RajeevRanjansing@users.noreply.github.com>
2024-05-14 22:43:05 +0530

    static block.java

---
+++ static block.java
@@ -0,0 +1,35 @@
+//1st program 
+
+class A{
+    
+    static{
+                System.out.println("Rajeev kumar singh");
+    }
+    public static void main(String args[]){
+                            //static block is executed without making object 
+        }
+}                      //output is Rajeev kumar singh 
+
+//2nd program
+
+class A{
+    int a=10; static int b=20;
+    static{
+                System.out.println(a + " "+ b);     //here we can use only static variable
+    }
+    public static void main(String args[]){
+                            //static block is executed without making object 
+        }
+}                                    //output is error, we can not use non-static variable inside static method
+
+//3rd program
+
+class B{
+    int a=10; static int b=20;
+    static{
+                System.out.println(b + " ");
+    }
+    public static void main(String args[]){
+                            //static block is executed without making object 
+        }
+}                                    //output is 20
