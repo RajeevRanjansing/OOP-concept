1c7ff7999abb1330965535e0eea4aeb39d8f5d36
Rajeev Ranjan <159820641+RajeevRanjansing@users.noreply.github.com>
2024-05-15 13:46:11 +0530

    Encapsulation.java

--- Encapsulation.java
+++ Encapsulation.java
@@ -1,1 +1,45 @@
+//Encapsulation is a mechanism through which we can binding the data members and members method in a single unit. 
+//we can access private data member using getter and setter method.
+//Getter method get the value.
+//setter method set the value. 
+class B{
+    private String name;
+    private int age;
+    private int roll;
+
+    public String getName(){
+        System.out.println(name);
+        return name;
+    }
+    public int getAge(){
+        System.out.println(age);
+        return age;
+    }
+    public int getRoll(){
+        System.out.println(roll);
+        return roll;
+    }
+
+    public void setName(String name){
+        this.name=name;
+    }
+    public void setAge(int age){
+        this.age=age;
+    }
+    public void setRoll(int roll){
+        this.roll=roll;
+    }
+
+    public static void main(String args[]){
+        B b=new B();
+            b.setName("Rajeev");
+            b.setAge(22);
+            b.setRoll(27);
+
+            b.getName();
+            b.getAge();
+            b.getRoll();
+        
+    } 
+}
 
